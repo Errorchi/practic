27ebfa3779825c64922b62d78598379726fddb2f
Errorchi <sykonnovegor@mail.ru>
2026-06-19 21:48:39 +0300

    fix: add Vite env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string;
+  readonly VITE_APP_TITLE?: string;
+  // Добавьте другие переменные окружения здесь
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
